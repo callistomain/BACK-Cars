c64622423a5aee0cd17c9d4f34867756b93bacf6
Alysson Marley Dias <callistomain@gmail.com>
2023-01-25 21:21:34 -0300

    core: first commit

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,44 @@
+import { prisma } from "../config/database.js";
+
+async function getCars() {
+  return await prisma.cars.findMany();
+}
+
+async function getCar(id: number) {
+  return await prisma.cars.findUnique({
+    where: { id }
+  })
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  return prisma.cars.findUnique({
+    where: { licensePlate }
+  })
+}
+
+async function createCar(model: string, licensePlate: string, year: number, color: string) {
+  return await prisma.cars.create({
+    data: {
+      model,
+      licensePlate,
+      year: year.toString(),
+      color
+    }
+  })
+}
+
+async function deleteCar(id: number) {
+  return await prisma.cars.delete({
+    where: { id }
+  })
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar
+}
+
+export default carRepository;
